b6fb2235028d0c6539abcb38574122ed9e4949be
kmacprt <72274248+kmacprt@users.noreply.github.com>
2020-12-31 09:47:42 -0800

    Update and rename sample_config.py to config.py

--- config.py
+++ config.py
@@ -2,17 +2,17 @@ import os
 
 class Config(object):
     # get a token from https://chatbase.com
-    CHAT_BASE_TOKEN = os.environ.get("CHAT_BASE_TOKEN", "")
+    CHAT_BASE_TOKEN = os.environ.get("CHAT_BASE_TOKEN", "d860f08e-4591-459f-98bb-75b54ab8199f")
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "1404216206:AAHGMuOUOXEFKv5PPJP_S6oUGtq0jmgvPFc")
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 12345))
+    APP_ID = int(os.environ.get("APP_ID", 1733305))
     API_HASH = os.environ.get("API_HASH")
     # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
     # Banned Unwanted Members..
-    BANNED_USERS = [1062346324,983706293,1134906983,578690785,1015727732,841188789]
+    BANNED_USERS = [1156597097]
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Telegram maximum file upload size
