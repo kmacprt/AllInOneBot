67542d10a52a64dc0e2cb0273399381a92c2f089
kmacprt <72274248+kmacprt@users.noreply.github.com>
2020-12-31 09:52:45 -0800

    Update config.py

--- config.py
+++ config.py
@@ -10,9 +10,9 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH")
     # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1156597097").split())
     # Banned Unwanted Members..
-    BANNED_USERS = [1156597097]
+    BANNED_USERS = []
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Telegram maximum file upload size
